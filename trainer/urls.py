5a09cc540b23598793bec57eeb077c4ec3d1637f
cmgoersch <goersch@madways.de>
2024-10-07 15:24:31 +0200

    change words implemented

--- trainer/urls.py
+++ trainer/urls.py
@@ -6,10 +6,12 @@ urlpatterns = [
     path('', views.wort_satz_liste, name='wort_satz_liste'),
     path('wort-hinzufuegen/', views.wort_hinzufuegen, name='wort_hinzufuegen'),
     path('satz-hinzufuegen/', views.satz_hinzufuegen, name='satz_hinzufuegen'),
-
-    # URLs für das Löschen
-    path('wort-loeschen/<int:id>/', views.wort_loeschen, name='wort_loeschen'),
-    path('satz-loeschen/<int:id>/', views.satz_loeschen, name='satz_loeschen'),
+    path('wort-loeschen/<int:pk>/', views.wort_loeschen, name='wort_loeschen'),
+    path('satz-loeschen/<int:pk>/', views.satz_loeschen, name='satz_loeschen'),
+    
+    # URLs für das Bearbeiten
+    path('wort-bearbeiten/<int:pk>/', views.wort_bearbeiten, name='wort_bearbeiten'),
+    path('satz-bearbeiten/<int:pk>/', views.satz_bearbeiten, name='satz_bearbeiten'),
 ]
 
 if settings.DEBUG:
--- trainer/views.py
+++ trainer/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
-from .models import Wort, Satz
-from .forms import WortForm, SatzForm
 from django.shortcuts import render, redirect, get_object_or_404
+from .models import Wort, Satz, Kategorie
+from .forms import WortForm, SatzForm
+
 
 def wort_satz_liste(request):
     wörter = Wort.objects.all()  # Hol nur die Wörter
@@ -43,3 +43,34 @@ def satz_loeschen(request, id):
     satz = get_object_or_404(Satz, id=id)
     satz.delete()  # Löschen des Satzes
     return redirect('wort_satz_liste')  # Zurück zur Liste nach dem Löschen
+
+
+# Bearbeiten eines Wortes
+def wort_bearbeiten(request, pk):
+    wort = get_object_or_404(Wort, pk=pk)
+    if request.method == 'POST':
+        form = WortForm(request.POST, instance=wort)
+        if form.is_valid():
+            wort = form.save(commit=False)
+            wort.kategorie = Kategorie.objects.get(name='Wort')  # Kategorie bleibt Wort
+            wort.save()
+            return redirect('wort_satz_liste')
+    else:
+        form = WortForm(instance=wort)
+    
+    return render(request, 'trainer/wort_bearbeiten.html', {'form': form})
+
+# Bearbeiten eines Satzes
+def satz_bearbeiten(request, pk):
+    satz = get_object_or_404(Satz, pk=pk)
+    if request.method == 'POST':
+        form = SatzForm(request.POST, instance=satz)
+        if form.is_valid():
+            satz = form.save(commit=False)
+            satz.kategorie = Kategorie.objects.get(name='Satz')  # Kategorie bleibt Satz
+            satz.save()
+            return redirect('wort_satz_liste')
+    else:
+        form = SatzForm(instance=satz)
+    
+    return render(request, 'trainer/satz_bearbeiten.html', {'form': form})
