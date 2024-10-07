83927446f8e4087cc037dd7e074b94bf0b1cdd68
cmgoersch <goersch@madways.de>
2024-10-07 12:10:00 +0200

    some styles

---
+++ trainer/static/js/script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const addButton = document.querySelector('a');
+    
+    addButton.addEventListener('click', function (event) {
+        alert('Du wirst nun ein neues Wort hinzufügen!');
+    });
+});
--- vokabeltrainer/settings.py
+++ vokabeltrainer/settings.py
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Verzeichnis für deine CSS- und JS-Dateien
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
