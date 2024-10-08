cbd7b06cb8b1b28369e857d3c52362ea79f95ffa
cmgoersch <goersch@madways.de>
2024-10-08 14:18:12 +0200

    vocable game works

--- trainer/models.py
+++ trainer/models.py
@@ -11,18 +11,18 @@ class Wort(models.Model):
     id = models.AutoField(primary_key=True)  # Explizite ID für Wort
     deutsch = models.CharField(max_length=100)
     finnisch = models.CharField(max_length=100)
-    merksatz = models.TextField(null=True, blank=True)  # Neues Feld für den Merksatz
-    kategorie = models.ForeignKey(Kategorie, on_delete=models.CASCADE, null=True, default=1)
+    merksatz = models.TextField(null=True, blank=True)
+    kategorie = models.ForeignKey(Kategorie, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.deutsch} - {self.finnisch}'
 
 class Satz(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)  # Explizite ID für Satz
     deutsch = models.CharField(max_length=255)
     finnisch = models.CharField(max_length=255)
     merksatz = models.TextField(null=True, blank=True)
-    kategorie = models.ForeignKey(Kategorie, on_delete=models.CASCADE, null=True, default=1)  # null=True vorübergehend
+    kategorie = models.ForeignKey(Kategorie, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.deutsch} - {self.finnisch}'
