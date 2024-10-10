d83aca94c3f1aac830c5e5f257b9868f9a932317
cmgoersch <goersch@madways.de>
2024-10-10 22:21:43 +0200

    works

--- vokabeltrainer/settings.py
+++ vokabeltrainer/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-7!jc^t!qfj^#f=jfj47)ubatoq66-k=v#@8nyft7y36w$5nr)q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['192.168.178.200', 'localhost', '127.0.0.1']
 
 # Application definition
 
